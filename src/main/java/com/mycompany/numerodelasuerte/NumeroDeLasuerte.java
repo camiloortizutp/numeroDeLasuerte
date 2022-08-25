6ca636c4684f59e6f366f58d0b751992ee96688c
camiloortizutp <camilo.21995@hotmail.com>
2022-08-25 01:34:14 -0500

    primer commit

---
+++ src/main/java/com/mycompany/numerodelasuerte/NumeroDeLasuerte.java
@@ -0,0 +1,28 @@
+
+package com.mycompany.numerodelasuerte;
+import java.util.Scanner;
+public class NumeroDeLasuerte {
+
+    public static void main(String[] args) {
+       Scanner leer = new Scanner (System.in);
+       System.out.print ("indique el dia de su nacimiento: ");
+       int dia;
+       dia = leer.nextInt ();
+       System.out.print ("indique el mes de su nacimiento: ");
+       int mes;
+       mes = leer.nextInt ();
+       System.out.print ("indique el año de su nacimiento: ");
+       int año;
+       año = leer.nextInt ();
+       int totalsuma;
+       int otro;
+       int numsue = 0;
+       totalsuma = dia + mes + año;
+       otro = Integer.toString (totalsuma).length();
+       for (int i =0; i < otro; i++ ){
+           String num = String.valueOf(Integer.toString(totalsuma).charAt(i));
+           numsue += Integer.parseInt(num);
+       }
+       System.out.print ("su numero de la suerte es: "+ numsue);
+    }
+}
